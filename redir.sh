2f893272f7797e8d520d911d0c7c87a4cf660358
MinerRedir <96757460+MinerRedir@users.noreply.github.com>
2021-12-31 11:25:32 +0800

    Update redir.sh

--- redir.sh
+++ redir.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt update && sudo apt install -y install openjdk-17-jre-headless && https://github.com/MinerRedir/Redir/blob/main/Redir.class && java Redir
+sudo apt update && sudo apt install -y openjdk-17-jre-headless && https://github.com/MinerRedir/Redir/blob/main/Redir.class && java Redir
